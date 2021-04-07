11dfaa6c25b761ce6b2a60de1595064af4c74bd1
GyuseokOh <rbtjr970525@gmail.com>
2021-04-07 20:54:37 +0900

    server_db_test

--- test/test/models/user.js
+++ test/test/models/user.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    /*userName: {
+    userName: {
         type: String,
         required: true,
-    },*/
+    },
     id: {
         type: String,
         required: true,
